dbf3127c91800893280047007100abb4604096cd
ArgTang <ArgTang@outlook.com>
2015-04-08 13:34:38 +0200

    test

--- src/CompanyLogic/Company.java
+++ src/CompanyLogic/Company.java
@@ -4,9 +4,5 @@ package CompanyLogic;
  * Created by steinar on 27.03.2015.
  */
 public final class Company {
-<<<<<<< HEAD
-
     private final String bossName = "Steinar";
-=======
->>>>>>> b9ce437f2c12e0a2979356ba12ddc6d10fce3b26
 }
