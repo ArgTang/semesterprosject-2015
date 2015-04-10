fdcfc25cc5254b3e45c94121ae6ba6571fde8ed0
ArgTang <ArgTang@outlook.com>
2015-04-10 11:52:36 +0200

    Added Witness Class

---
+++ src/Person/Witness.java
@@ -0,0 +1,19 @@
+package Person;
+
+/**
+ * Created by steinar on 10.04.2015.
+ */
+public class Witness {
+
+    String firstName;
+    String Lastname;
+    ContactInfo contactInfo;
+
+
+    //temp Constructor for compiling
+    public Witness(String firstName, String lastname, ContactInfo contactInfo) {
+        this.firstName = firstName;
+        Lastname = lastname;
+        this.contactInfo = contactInfo;
+    }
+}
