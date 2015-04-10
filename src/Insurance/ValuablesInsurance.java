be3d45cdbe269ca0d15ef65ffc44a5b8484bf69a
Zelteck <alek.hdal@gmail.com>
2015-04-10 11:58:01 +0200

    La til TODO;

--- src/Insurance/ValuablesInsurance.java
+++ src/Insurance/ValuablesInsurance.java
@@ -3,7 +3,8 @@ package Insurance;
 /**
  * Created by steinar on 27.03.2015.
  */
-public final class ValuablesInsurance extends Insurance{
+public final class ValuablesInsurance extends Insurance //TODO: Change class name to something more appropriate, this class is an insurance for a specific high value item. For instance a 100.000,- guitar
+{
 
     //temp constructor (for compile)
     //TODO: low priority: only if we have time to implement this
