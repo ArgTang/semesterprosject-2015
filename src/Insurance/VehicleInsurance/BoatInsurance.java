e569d24542fafe46398f41957ac1dc4b84b3d743
ArgTang <ArgTang@outlook.com>
2015-04-11 20:59:45 +0200

    added boat object to boat insurance

--- src/Insurance/VehicleInsurance/BoatInsurance.java
+++ src/Insurance/VehicleInsurance/BoatInsurance.java
@@ -1,18 +1,19 @@
 package Insurance.VehicleInsurance;
 
 import Objects.Vehicle;
+import Objects.VehicleBoat;
 
 /**
  * Created by steinar on 27.03.2015.
  */
 public final class BoatInsurance extends VehicleInsurance {
 
-    //int feet; //todo: boat class?
+
+    VehicleBoat Boat;
 
     //temp constructor (for compile)
 
     public BoatInsurance(double insuranseValue, String insurancePolicy, Vehicle vehicle, int feet) {
         super(insuranseValue, insurancePolicy, vehicle);
-       // this.feet = feet;
     }
 }
--- src/Insurance/VehicleInsurance/MotorBike.java
+++ src/Insurance/VehicleInsurance/MotorBike.java
@@ -5,7 +5,7 @@ import Objects.Vehicle;
 /**
  * Created by steinar on 07.04.2015.
  */
-public class MotorBike extends VehicleInsurance {
+public final class MotorBike extends VehicleInsurance {
 
     //temp constructor (for compile
     public MotorBike(double insuranseValue, String insurancePolicy, Vehicle vehicle) {
