b030419065d41776b3c3b4af9888350224c05164
ArgTang <ArgTang@outlook.com>
2015-04-15 13:42:31 +0200

    ny husforsikring vindu

--- src/Objects/Property.java
+++ src/Objects/Property.java
@@ -14,13 +14,13 @@ public final class Property {
     private String city;
 
     private short constructionYear;
-    private String buildingMaterial;    //todo: ENUM?
+    private String buildingMaterial;    //todo: ENUM? mur, tre, betong osv
     private int buildingValue;
-    private String type;             //todo: need this? ENUM?
+    private String type;             //todo: need this? ENUM? rekkehus, blokk, enebolig osv
     private short floors;
     private int totalSqm;
 
-    private Person Owner;           //todo: what if a company owns this?
+    private Person Owner;           //todo: what if a company owns this? change to Person key
     private List<Person> contactPerson; //todo: List< keyfromCustomerRegister > instead -> no doublestorage.
     List<File> pictures;
 
